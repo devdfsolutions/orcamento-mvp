f846a91b578886ca23ab4af6819c73ffb2a2d2d7
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-14 11:29:21 -0300

    Update page.tsx

--- app/cadastros/produtos/page.tsx
+++ app/cadastros/produtos/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const runtime = "nodejs";
 import React from 'react';
 import ConfirmSubmit from '@/components/ConfirmSubmit';
 import AutoCloseForm from '@/components/AutoCloseForm';
