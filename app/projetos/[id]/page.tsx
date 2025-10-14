88f090213ba50b79b265403416a691bf9ddaf243
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-14 11:30:47 -0300

    Update page.tsx

--- app/projetos/[id]/page.tsx
+++ app/projetos/[id]/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const runtime = "nodejs";
 import { prisma } from '@/lib/prisma';
 import { getSupabaseServer } from '@/lib/supabaseServer';
 import { redirect } from 'next/navigation';
