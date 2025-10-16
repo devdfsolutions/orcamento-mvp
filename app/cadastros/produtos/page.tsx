cba10bef2a5fc98a6e9ab1deee9f0159eb0064ae
Daniel Ferreira <devdfsolutions@gmail.com>
2025-10-16 09:04:27 -0300

    Update page.tsx

--- app/cadastros/produtos/page.tsx
+++ app/cadastros/produtos/page.tsx
@@ -152,7 +152,7 @@ export default async function Page() {
           <colgroup>
             <col style={{ width: 30 }} />
             <col />
-            <col style={{ width: 200 }} />
+            <col style={{ width: 60 }} />
             <col style={{ width: 70 }} />
             <col style={{ width: 140 }} />
             <col style={{ width: 110 }} />
